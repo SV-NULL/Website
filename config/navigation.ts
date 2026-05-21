19c3db193924d861394db66e007ced1404dd4379
dennisw05 <49673324+dennisw05@users.noreply.github.com>
2026-05-21 12:38:24 +0200

    fix: update navigation structure to better serve distinct target audiences

--- config/navigation.ts
+++ config/navigation.ts
@@ -2,31 +2,22 @@ import { NavItem, SocialNavItem } from "@/types/image";
 import { Github, Instagram, Linkedin } from "lucide-react";
 
 export const navItems: NavItem[] = [
-  {
-    name: "Vereniging",
-    href: "/bestuur",
-    sub: [
-      { name: "Word lid", href: "/word-lid" },
-      { name: "Bestuur", href: "/bestuur" },
-      { name: "Commissies", href: "/commissies" },
-      { name: "Over ons", href: "/over-ons" },
-    ],
-  },
+  { name: "Over ons", href: "/over-ons" },
   { name: "Kalender", href: "/kalender" },
   {
     name: "Opleiding",
-    href: "/opleiding/vakken",
     sub: [
       { name: "Vakken", href: "/vakken" },
+      { name: "Vacatures", href: "/vacatures" },
       { name: "Studentenwelzijn", href: "/studentenwelzijn" },
+      { name: "Bestuur", href: "/bestuur" },
+      { name: "Commissies", href: "/commissies" },
     ],
   },
   {
     name: "Partners",
-    href: "/partners",
     sub: [
       { name: "Huidige partners", href: "/partners" },
-      { name: "Vacatures", href: "/vacatures" },
       { name: "Partner worden", href: "/partner-worden" },
     ],
   },
